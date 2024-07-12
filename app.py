bdce2adb1df1d7b40279334444be875f83f42e7d
Rahul <127227338+rahulns1993@users.noreply.github.com>
2024-07-12 20:05:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,10 @@
-from flask import Flask, request
-from app_service import AppService
-import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
-appService = AppService();
-
 
 @app.route('/')
 def home():
-    return "App Works!!!"
+    return render_template('index.html')
 
-
-@app.route('/api/tasks')
-def tasks():
-    return appService.get_tasks()
-
-@app.route('/api/task', methods=['POST'])
-def create_task():
-    request_data = request.get_json()
-    task = request_data['task']
-    return appService.create_task(task)
-
-
-@app.route('/api/task', methods=['PUT'])
-def update_task():
-    request_data = request.get_json()
-    return appService.update_task(request_data['task'])
-
-
-@app.route('/api/task/<int:id>', methods=['DELETE'])
-def delete_task(id):
-    return appService.delete_task(id)
-
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
